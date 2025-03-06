4234c803587448cb794c13da74f0fafd2470dc85
Valentin <valentinmartinez2604@gmail.com>
2025-03-06 16:21:28 -0300

    Se modificó la función agregarAmigo y se creó la función recorrerLista

--- app.js
+++ app.js
@@ -3,16 +3,12 @@
 let amigos = []
 
 function agregarAmigo(){
+    
     let nombre = document.getElementById("amigo").value;
-    console.log(nombre);
+    
     if (nombre === "") {
         alert("Por favor, inserte un nombre.");
-    } else if (nombre === Number){
-        alert("Por favor, inserte un nombre valido.")
-    } else if(nombre in amigos){
-        alert("Este nombre ya ha sido ingresado.")
-    }
-    else {
+    } else {
         amigos.push(nombre);
         document.getElementById("amigo").value = "";
     }
